e2f93188f7f0cb0b8e9231e4767514872368c75f
Will Roberts <will.roberts11@gmail.com>
2017-11-19 13:17:17 -0800

    Updates item model

--- items/items.go
+++ items/items.go
@@ -4,11 +4,14 @@ package items
 type Item struct {
 	Corrupted         bool
 	ExplicitMods      []string
-	FlavorText        []string
-	Height            int64  `json:"h"` // In stash spaces.
-	Icon              string // URL.
+	FlavorText        []string `json:"flavourText"`
+	FrameType         int64
+	Height            int64 `json:"h"` // In stash spaces.
 	ID                string
+	Icon              string // URL.
 	Identified        bool
+	ImplicitMods      []string
+	InventoryID       string
 	ItemLevel         int64 `json:"ilvl"`
 	League            string
 	LockedToCharacter bool // Chinese realm only.
@@ -16,18 +19,15 @@ type Item struct {
 	Note              string
 	Properties        []Property
 	Requirements      []Requirement
+	SocketedItems     interface{}
 	Sockets           []Socket
+	Support           bool
 	TalismanTier      int64
 	TypeLine          string // Base type.
 	Verified          bool
 	Width             int64 `json:"w"` // In stash spaces.
 	X                 int64 // Position in stash.
 	Y                 int64 // Position in stash.
-
-	FrameType     int64       // Unknown.
-	InventoryID   string      // Unknown.
-	SocketedItems interface{} // See URL above.
-	Support       bool        // Unknown.
 }
 
 // Property stores a pair of name and values.
