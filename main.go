6bd34738ee30759ccd8d5f4d8d044cfcfadeffa4
Will Roberts <will.roberts11@gmail.com>
2017-11-19 01:45:37 -0800

    Run continuously

--- main.go
+++ main.go
@@ -14,21 +14,25 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to parse settings: ", err)
 	}
+
 	next := settings.Get("settings.next_change_id").(string)
 	log.Println("Initial change ID: ", next)
 
 	c := client.NewClient()
 
-	start := time.Now()
-	resp, err := c.GetStashes(next)
-	if err != nil {
-		log.Fatal("failed to retrieve stashes: ", err)
-	}
-	latency := time.Since(start)
+	for {
+		start := time.Now()
+		resp, err := c.GetStashes(next)
+		if err != nil {
+			log.Fatal("failed to retrieve stashes: ", err)
+		}
+		latency := time.Since(start)
 
-	log.Println("Stashes:", len(resp.Stashes))
-	log.Println("Next Change ID:", resp.NextChangeID)
-	log.Println("Latency:", latency)
+		log.Println("Stashes:", len(resp.Stashes))
+		log.Println("Next Change ID:", resp.NextChangeID)
+		next = resp.NextChangeID
+		log.Println("Latency:", latency)
+	}
 }
 
 func readSettings() string {
