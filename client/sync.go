3ff259a7f1778c3c035edfe3412513ef6c47c1e7
Will Roberts <will.roberts11@gmail.com>
2017-11-19 01:45:54 -0800

    Adds change ID backend

---
+++ client/sync.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	ChangeIDURL string = "http://poe.ninja/api/Data/GetStats"
+)
+
+type NinjaStats struct {
+	NextChangeID string `json:"next_change_id"`
+}
+
+func GetLatestChangeID() string {
+	resp, err := http.Get(ChangeIDURL)
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+
+	var stats NinjaStats
+	err = json.Unmarshal(b, &stats)
+	if err != nil {
+		return err.Error()
+	}
+
+	return stats.NextChangeID
+}
---
+++ client/sync_test.go
@@ -0,0 +1,13 @@
+package client
+
+import (
+	"testing"
+)
+
+func TestGetLatestChangeID(t *testing.T) {
+	out := GetLatestChangeID()
+	if out == "" {
+		t.Log("Failed to retrieve current change ID")
+		t.FailNow()
+	}
+}
