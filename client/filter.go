76b522d5b542db86fb0daff50e77521bcb96d5bf
Will Roberts <will.roberts11@gmail.com>
2017-11-19 12:55:16 -0800

    Improves output

--- client/filter.go
+++ client/filter.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"log"
+
+	"github.com/willroberts/perandus/filter"
 )
 
 func (c *client) runFilterWorker() {
@@ -13,8 +15,15 @@ func (c *client) runFilterWorker() {
 		}
 
 		for _, i := range s.Items {
-			if c.Filter.Matches(i) {
-				log.Println("ALERT! Found matching item:", i)
+			// Strip localization tags from item names before proceeding.
+			// It may be better to do this elsewhere.
+			i.Name = filter.StripLocalizationTags(i.Name)
+
+			if c.Filter.Matches(i) && i.Note != "" {
+				log.Println("ALERT! Found matching item:")
+				log.Println("\tName:", i.Name)
+				log.Println("\tPrice:", i.Note)
+				log.Println("\tSeller:", s.LastCharacterName)
 			}
 		}
 	}
