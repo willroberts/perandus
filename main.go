eed7735fde5eb2c5956338fca1be1221ca9a7f8c
Will Roberts <will.roberts11@gmail.com>
2017-11-21 02:13:21 -0800

    Updates usage of filters

--- main.go
+++ main.go
@@ -5,22 +5,28 @@ import (
 
 	"github.com/willroberts/perandus/alert"
 	"github.com/willroberts/perandus/client"
+	"github.com/willroberts/perandus/filter"
 	"github.com/willroberts/perandus/util"
 )
 
 func main() {
 	latest := util.GetLatestChangeID()
 	log.Printf("Starting from change ID %s", latest)
+
 	c := client.New(latest)
 	itemCh, errCh := c.Poll()
-	//create filter
+
+	f, err := filter.New()
+	if err != nil {
+		log.Fatal("error creating filter:", err.Error())
+	}
+
+	filteredCh := f.FilterItems(itemCh)
+
 	for {
 		select {
-		case i := <-itemCh:
-			//if filter.matches() && i.Note != "" {
-			if i.Note != "" {
-				alert.ConsoleLogAlert(i)
-			}
+		case i := <-filteredCh:
+			alert.ConsoleLogAlert(i)
 		case err := <-errCh:
 			log.Fatal("error during polling:", err.Error())
 		}
